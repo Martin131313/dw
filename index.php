767218cc96e7a611c55058f07f1b110e67566fcd
Martin131313 <moc@semosedu.local>
2017-03-01 12:39:20 +0100

    yeah4

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- GET e array(niza) -->
+<!-- count se koristi kaj arrays i dava kolku elementi ima vo nizata -->
+<!-- text editor (Netbeans) -->
+<!-- empty Returns FALSE if var exists and has a non-empty, non-zero value. Otherwise returns TRUE. -->
+<!-- isset Returns TRUE if var exists and has value other than NULL. FALSE otherwise.-->
+
+<?php
+
+// var_dump($_GET);
+// echo '<br/>';
+// var_dump(count($_GET));
+// echo '<br/>';
+
+// var_dump($_GET);
+// exit;
+
+// if (count($_GET) > 0){
+// if(!empty($_GET)){	
+if(isset($_GET['pin1']) && is_numeric($_GET['pin1']) && isset($_GET['pin2']) && is_numeric($_GET['pin2'])){
+	if($_GET['pin1'] == $_GET['pin2']){
+		echo 'Validni i isti PIN-ovi :)<br/>';
+	} else {
+		echo 'Validen ama razlichni PIN-ovi :)<br/>';
+	}
+}
+else {
+	echo 'Nevalidni PIN-ovi :(<br/>';
+}
+
+
+echo '<a href="index_html.php">LINK</a>';
+
+
+  ?>
---
+++ index_html.php
@@ -0,0 +1,11 @@
+<form method="get" action="index.php">
+Username:
+<input type="text" placeholder="enter username" name="username" /></br>
+Password:
+<input type="text" placeholder="enter password" name="password" /></br>
+Enter PIN:
+<input type="text" placeholder="enter 4 digit number" name="pin1" /></br>
+Confirm PIN:
+<input type="text" placeholder="confirm pin" name="pin2" /></br>
+<input type="submit" value="Login"></br>
+</form>
